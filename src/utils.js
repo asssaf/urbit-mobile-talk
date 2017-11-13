f86a3694d0084ff16dc3d21b0a7e78942accf05a
assaf <asssaf@localhost>
2017-11-12 17:17:26 -0800

    limit log size

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
+
 // override console log function
 (function(){
+  const LOG_SIZE = 1000
   console.items = []
   var orig = {
     log: console.log,
@@ -9,6 +11,10 @@
   Object.keys(orig).forEach(k => {
     console[k] = function() {
       orig[k].apply(console, arguments)
+
+      if (console.items.length == LOG_SIZE) {
+        console.items = console.items.slice(1)
+      }
 
       console.items.push({
         type: k,
