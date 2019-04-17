f0a0d09442f93c37eab68db63ccfe065ce3e8a86
ryjm <jraydermiller@gmail.com>
2019-04-16 21:42:46 -0400

    upgrade to expo-cli and some small fixes

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { DrawerNavigator, NavigationActions, StackNavigator } from 'react-navigation';
+import { createAppContainer, createDrawerNavigator, NavigationActions, StackActions, createStackNavigator } from 'react-navigation';
 import { webapi as urbit } from '@asssaf/urbit';
 import Login from './Login';
 import LoadingScreen from './LoadingScreen';
@@ -12,7 +12,7 @@ import { } from './utils';
 import { formatShip } from './formatting'
 import { loadState, saveState } from './persistence'
 
-const ChatNavigator = StackNavigator({
+const ChatNavigator = createStackNavigator({
   Loading: {
     screen: LoadingScreen,
   },
@@ -38,13 +38,14 @@ const ChatNavigator = StackNavigator({
   },
 })
 
-const MenuNavigator = DrawerNavigator({
+const MenuNavigator = createDrawerNavigator({
   Chat: {
     screen: ChatNavigator,
   },
 }, {
   contentComponent: ChatMenu,
 })
+const AppContainer = createAppContainer(MenuNavigator, ChatNavigator);
 
 export default class Main extends React.Component {
   state = {
@@ -64,7 +65,7 @@ export default class Main extends React.Component {
   }
 
   switchToLogin() {
-    var action = NavigationActions.reset({
+    var action = StackActions.reset({
       index: 0,
       actions: [
         NavigationActions.navigate({
@@ -80,7 +81,7 @@ export default class Main extends React.Component {
   }
 
   switchToChat() {
-    var action = NavigationActions.reset({
+    var action = StackActions.reset({
       index: 0,
       actions: [
         NavigationActions.navigate({
@@ -167,10 +168,15 @@ export default class Main extends React.Component {
     }
 
     return (
-      <View style={styles.container}>
+      <AppContainer
+        ref={nav => {
+          this.navigator = nav;
+        }}
+        screenProps={screenProps}>
+        <View style={styles.container}>
         <View style={styles.header} />
-        <MenuNavigator screenProps={screenProps} ref={(nav) => {this.navigator = nav; } } />
-      </View>
+        </View>
+      </AppContainer>
     )
   }
 }
